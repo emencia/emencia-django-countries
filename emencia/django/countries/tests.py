45820be1ecfb08a8956ebf2ea764ca4a143fdf36
Fantomas42 <fantomas42@gmail.com>
2010-02-17 10:46:28 +0100

    adding tests

--- emencia/django/countries/tests.py
+++ emencia/django/countries/tests.py
@@ -8,5 +8,20 @@ class CountryTestCase(TestCase):
     """Tests for Country model"""
 
     def test_manager(self):
-        pass
-    
+        countries = Country.objects.all()
+        self.assertEquals(countries.count(), 244)
+        self.assertEquals(countries[0], Country.objects.get(iso='AF'))
+
+        countries = Country.objects.leveled()
+        self.assertEquals(countries.count(), 0)
+
+        france = Country.objects.get(iso='FR')
+        france.level = 100
+        france.save()
+
+        countries = Country.objects.leveled()
+        self.assertEquals(countries.count(), 1)
+
+        countries = Country.objects.all()
+        self.assertEquals(countries.count(), 244)
+        self.assertEquals(countries[0], france)
