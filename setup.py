025ac7e0eafa4c6c2a50b0b6f221e8c0bc5c8ea1
Fantomas42 <fantomas42@gmail.com>
2010-03-12 10:26:31 +0100

    better classifier

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='emencia.django.countries',
           'Programming Language :: Python',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'License :: OSI Approved :: BSD License',
+          'Framework :: Django',
           ],
 
       author='Fantomas42',
