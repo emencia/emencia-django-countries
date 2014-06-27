9de67953e7601eef55fa0d54be32cd506ebf27f0
Carlos Solsona <carlos.solsona@gmail.com>
2014-06-27 17:16:03 +0200

    Update admin. Read the lang from settings

--- emencia/django/countries/admin.py
+++ emencia/django/countries/admin.py
@@ -1,15 +1,17 @@
 """Admin for emencia.django.countries"""
 from django.contrib import admin
 from django.utils.translation import ugettext as _
+from django.conf import settings
 
 from emencia.django.countries.models import Country
 
+fields = ['printable_name_%s' % lang for lang in settings.LANGUAGES ]
 
 class CountryAdmin(admin.ModelAdmin):
-    search_fields = ('printable_name_en', 'name', 'iso', 'iso3')
-    list_display = ('printable_name_en', 'name', 'iso', 'iso3', 'numcode','level')
+    search_fields = fields + ['name', 'iso', 'iso3']
+    list_display = fields + ['name', 'iso', 'iso3', 'numcode','level']
     ordering = ('printable_name_en',)
-    fieldsets = ((None, {'fields': ('printable_name_en','printable_name_fr', 'name')}),
+    fieldsets = ((None, {'fields': fields + ['name']}),
                  (_('References'), {'fields': ('iso', 'iso3', 'numcode')}),
                  (None, {'fields': ('level',)}),)
     actions_on_top = False
