6426edb7de56ebd501c3b0b1d993f4b7501a2e2e
J. David Ibáñez <jdavid.ibp@gmail.com>
2016-10-26 12:01:52 +0200

    Fix migrations

--- emencia/django/countries/migrations/0006_auto_20161021_1442.py
+++ emencia/django/countries/migrations/0006_auto_20161021_1442.py
@@ -11,29 +11,30 @@ def load_data(apps, schema_editor):
     The source file is http://sql.sh/ressources/sql-pays/sql-pays.csv
     Plus four more countries added manually to complete the list.
     """
-    Model = apps.get_model('countries', 'Country')
+    Country = apps.get_model('countries', 'Country')
     Translation = apps.get_model('countries', 'CountryTranslation')
 
-    path = abspath(join(__file__, '..', '..', 'fixtures', 'fr_data.csv'))
-
-    objects = []
+    path = abspath(join(__file__, '..', '..', 'fixtures', 'data.csv'))
     with open(path) as f:
-        for _, _, iso, _, name, _ in csv.reader(f):
-            try:
-                model = Model.objects.get(iso=iso)
-            except Model.DoesNotExist:
-                continue
+        for _, numcode, iso, iso3, fr, en in csv.reader(f):
+            defaults = {'iso3': iso3, 'numcode': numcode}
+            model, created = Country.objects.get_or_create(iso=iso,
+                                                           defaults=defaults)
 
-            objects.append(Translation(master=model, language_code='fr',
-                                       name=name.upper(), printable_name=name))
+            defaults = {'name': en.upper(), 'printable_name': en}
+            Translation.objects.get_or_create(master=model, language_code='en',
+                                              defaults=defaults)
 
-    if objects:
-        Translation.objects.bulk_create(objects)
+            defaults = {'name': fr.upper(), 'printable_name': fr}
+            Translation.objects.get_or_create(master=model, language_code='fr',
+                                              defaults=defaults)
+
 
 
 def unload_data(apps, schema_editor):
-    MyModelTranslation = apps.get_model('countries', 'CountryTranslation')
-    MyModelTranslation.objects.filter(language_code='fr').delete()
+    Translation = apps.get_model('countries', 'CountryTranslation')
+    Translation.objects.filter(language_code='en').delete()
+    Translation.objects.filter(language_code='fr').delete()
 
 
 class Migration(migrations.Migration):
